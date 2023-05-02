63e0df341ed0d632229273bfb559a977f06015b0
mim2041 <suraiyamim338@gmail.com>
2023-05-02 20:25:13 +0600

    data are fetched

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+const allRecipes = require("./data.json");
 const port = process.env.PORT || 5000;
 
 app.get('/',  (req, res) => {
-    res.send("Hello Bengali Cuisine");
+    res.send("Hello from Bengali Cuisine");
 });
+
+app.get("/allRecipes", (req, res) => {
+    res.send(allRecipes);
+})
 
 app.listen(port, () => {
     console.log(`Bengali cuisine is running on port ${port}`)
